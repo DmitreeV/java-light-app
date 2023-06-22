4544907e6f7ef946de8cb3057ec780c372595d14
Dmitree <color.ivanov2016@yandex.ru>
2023-06-22 20:09:12 +0300

    init commit

---
+++ src/main/java/dmitreev/testwork/javalightapp/JavaLightAppApplication.java
@@ -0,0 +1,13 @@
+package dmitreev.testwork.javalightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLightAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLightAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitreev/testwork/javalightapp/JavaLightAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitreev.testwork.javalightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLightAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
