c617586d57404bea6a71659b8c1fcdc883756306
Dmitree <color.ivanov2016@yandex.ru>
2023-06-24 18:18:37 +0300

    solving the mapping problem during assembly

--- src/main/java/dmitreev/testwork/javalightapp/mapper/ContractMapper.java
+++ src/main/java/dmitreev/testwork/javalightapp/mapper/ContractMapper.java
@@ -3,11 +3,16 @@ package dmitreev.testwork.javalightapp.mapper;
 import dmitreev.testwork.javalightapp.dto.ContractDto;
 import dmitreev.testwork.javalightapp.model.Contract;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ContractMapper {
 
+    @Mapping(target = "admin", source = "admin.id")
+    @Mapping(target = "principal", source = "principal.inn")
     ContractDto toContractDto(Contract contract);
 
+    @Mapping(target = "admin.id", source = "admin")
+    @Mapping(target = "principal.inn", source = "principal")
     Contract toContract(ContractDto contractDto);
 }
--- src/main/java/dmitreev/testwork/javalightapp/mapper/EventMapper.java
+++ src/main/java/dmitreev/testwork/javalightapp/mapper/EventMapper.java
@@ -3,11 +3,14 @@ package dmitreev.testwork.javalightapp.mapper;
 import dmitreev.testwork.javalightapp.dto.EventDto;
 import dmitreev.testwork.javalightapp.model.Event;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
 
+    @Mapping(target = "admin", source = "admin.id")
     EventDto toAdminDto(Event event);
 
+    @Mapping(target = "admin.id", source = "admin")
     Event toEvent(EventDto eventDto);
 }
